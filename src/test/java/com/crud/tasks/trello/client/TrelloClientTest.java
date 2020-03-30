20ace187b72c100eb405eedeb9eb7ccbb26f227d
rjagodzki <jagodzki.radek@outlook.com>
2020-03-30 19:44:38 +0200

    Zadanie 22.4, ale problem ze wczesniejszymi testami

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -88,4 +88,18 @@ public class TrelloClientTest {
         assertEquals("http://test.com", newCard.getShortUrl());
     }
 
+    @Test
+    public void shouldReturnEmptyList() throws URISyntaxException {
+        //Given
+        URI uri = new URI("http://test.com/members/radekjagodzki/boards?key=test&token=test&fields=name,id&lists=all");
+
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(null);
+
+        //When
+        List<TrelloBoardDto> emptyList = trelloClient.getTrelloBoards();
+
+        //Then
+        assertEquals(0, emptyList.size());
+    }
+
 }
